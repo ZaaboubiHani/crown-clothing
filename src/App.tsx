2f5f73f61af159a6d1be24200c9d679e87d7ec06
unknown <nilzak.indigo05@gmail.com>
2023-07-28 15:50:51 +0100

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
-      <Routes>
-        <Route path='/' Component={HomePage} />
-        <Route path='/shop' Component={ShopPage} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/shop' Component={ShopPage} />
+        </Routes>
+      </div>
     );
   }
 }
